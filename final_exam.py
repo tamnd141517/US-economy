1b6fa4652cb28ecf8c064a557739c726d0533081
tamnd141517 <131211324+tamnd141517@users.noreply.github.com>
2023-10-17 15:35:30 +0100

    Update final_exam.py

--- final_exam.py
+++ final_exam.py
@@ -33,6 +33,8 @@ data_1 = pd.read_csv("fred_md_desc.csv",index_col = 0)
 data_1.dropna(how ="all", inplace = True) #Drop lines from the file where all entries are NaN
 df_1 = data_1.drop(['fred'],axis = 1)
 
+#Mathematically transform data in order to fit the model. Produce a new data frame by applying 
+the specified mathematical transformation to each variable in the data
 desc = pd.DataFrame(df_1.values,index = data_1['fred'],columns = df_1.columns)
 desc.rename(index = {"VXOCLSx":"VIXCLSx"},inplace = True)
 for i in desc.index:
@@ -58,13 +60,16 @@ for i in desc.index:
     else:
         new_df[i] = (my_df[i]/my_df[i].shift(1)-1)
 
+#Standardise each transformed variable
 st_new_df = (new_df-new_df.mean())/new_df.std()
 st_new_df.fillna(0, inplace = True)
 st_new_df.to_csv('transformed_data.csv')
 
+#Produce a PCA analysis.
 pca_function(st_new_df)
 factor = pca_function(st_new_df)
 
+#Produce a histogram of the distribution of the factor
 sns.set_theme()
 fig, axes = plt.subplots(1,2, figsize=(14, 4))
 factor['comp_0'].plot(ax=axes[0])
@@ -73,9 +78,11 @@ fig.figure.suptitle('Primary Factor (1st Principal Component)')
 plt.tight_layout()
 fig.savefig('factor.pdf')
 
+#Produce a new data frame of 1st lags of the data 
 lagdt = new_df.shift(1)
 lagfactor = factor.shift(1)
 
+#Produce a regression model. Produce a data frame of the fitted values from the 5 models.
 srs_df = pd.DataFrame(index = new_df.index, columns = my_srs)
 for s in my_srs:
         reg_data = pd.concat([new_df[s],lagdt[s],lagfactor],axis =1).set_axis(['srs','lagsrs','lagfactor'],axis =1).dropna()
@@ -88,6 +95,7 @@ for s in my_srs:
 srs_df.dropna(how = 'all', inplace =True)
 srs_df.to_csv('fitted_values.csv')
 
+#Produce for each of the variables a Seaborn plot
 nber = pd.read_csv('NBER_DATES.csv',index_col=0)
 nber1 = nber['1959-03':'2019-12']
 nber1.index = pd.to_datetime(nber1.index,format='%Y/%m').to_period('M')
